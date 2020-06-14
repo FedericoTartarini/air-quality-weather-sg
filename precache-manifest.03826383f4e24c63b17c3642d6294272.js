448e28fb24150a4eaae05c96f59bb30c2c7abc30
FedericoTartarini <federicotartarini@gmail.com>
2020-06-14 22:38:35 +0800

    Updates

---
+++ precache-manifest.03826383f4e24c63b17c3642d6294272.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6488046a2b4efd2b05e4044c361c715",
+    "url": "/air-quality-weather-sg/index.html"
+  },
+  {
+    "revision": "fd96c16cec02687d5a21",
+    "url": "/air-quality-weather-sg/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "5c868ba1ab97043d9509",
+    "url": "/air-quality-weather-sg/static/css/main.cca977cc.chunk.css"
+  },
+  {
+    "revision": "fd96c16cec02687d5a21",
+    "url": "/air-quality-weather-sg/static/js/2.80af3272.chunk.js"
+  },
+  {
+    "revision": "7698ab7fef391e31a472e6dff7d9c09f",
+    "url": "/air-quality-weather-sg/static/js/2.80af3272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c868ba1ab97043d9509",
+    "url": "/air-quality-weather-sg/static/js/main.14ac5510.chunk.js"
+  },
+  {
+    "revision": "c810602bfc569800f40d",
+    "url": "/air-quality-weather-sg/static/js/runtime-main.f5dba31b.js"
+  },
+  {
+    "revision": "bcb615f1cac4810bcb6735a439e85568",
+    "url": "/air-quality-weather-sg/static/media/good-day.bcb615f1.jpg"
+  },
+  {
+    "revision": "52c8dcb0b4d425cf1b3ea24b843a0a2b",
+    "url": "/air-quality-weather-sg/static/media/hazy.52c8dcb0.png"
+  },
+  {
+    "revision": "f95ef2cbb0f26d3ddd84fed265c0f8d3",
+    "url": "/air-quality-weather-sg/static/media/logo.f95ef2cb.svg"
+  },
+  {
+    "revision": "d67ab07e604a4a18627db0eeaa1a5312",
+    "url": "/air-quality-weather-sg/static/media/singapore-line.d67ab07e.png"
+  }
+]);
