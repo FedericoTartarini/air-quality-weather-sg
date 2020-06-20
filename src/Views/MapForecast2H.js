b5ae0274b4941956778874414b11bb2f80c29000
FedericoTartarini <federicotartarini@gmail.com>
2020-06-20 21:53:38 +0800

    * added button to toggle view

--- src/Views/MapForecast2H.js
+++ src/Views/MapForecast2H.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MapWeather from "../Components/MapWeather";
+import DropDown from "../Components/DropDown";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronDown,
+  faToggleOff,
+  faToggleOn,
+} from "@fortawesome/free-solid-svg-icons";
 
 function MapForecast2H({ data }) {
+  const [toggle, setToggle] = useState(true);
+
+  // useEffect(() => {}, [data]);
+
   return (
     <div>
-      <MapWeather data={data} />
+      <div className="container mx-auto my-2 text-center">
+        {" "}
+        <button
+          className="bg-gray-100 hover:bg-gray-200 text-gray-800 font-bold p-2 rounded inline-flex items-center justify-evenly"
+          onClick={() => setToggle(!toggle)}
+        >
+          <span className="text-right w-32 mr-6">
+            {toggle ? "2-h Forecast" : "24-h Forecast"}
+          </span>
+          <FontAwesomeIcon
+            icon={toggle ? faToggleOn : faToggleOff}
+            size={"1x"}
+            className="mr-8"
+          />
+        </button>
+      </div>
+      <div>
+        <MapWeather data={data} />
+      </div>
     </div>
   );
 }
