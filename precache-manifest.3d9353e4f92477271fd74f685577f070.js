9edf99aa151ea93427e6166f8ea300787bd2abb3
FedericoTartarini <federicotartarini@gmail.com>
2020-11-07 15:58:42 +0800

    Updates

---
+++ precache-manifest.3d9353e4f92477271fd74f685577f070.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7342dbce7333928f5dd5ad46bbff35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bf72c6bcf5f5836aeec",
+    "url": "/static/css/4.7c35e809.chunk.css"
+  },
+  {
+    "revision": "a0395d657f6ae4e355bc",
+    "url": "/static/css/main.f1c4a49e.chunk.css"
+  },
+  {
+    "revision": "c47983d1afc4fa6ea46c",
+    "url": "/static/js/0.cbecec0e.chunk.js"
+  },
+  {
+    "revision": "054b3010cc0be41b6f6b",
+    "url": "/static/js/1.d5e7dcf7.chunk.js"
+  },
+  {
+    "revision": "cc4f340ec12a98ec5b57",
+    "url": "/static/js/10.bdaf07b3.chunk.js"
+  },
+  {
+    "revision": "a62ae7646e185633277a",
+    "url": "/static/js/11.3f05bd4d.chunk.js"
+  },
+  {
+    "revision": "39363ba4673ed2543f7d",
+    "url": "/static/js/12.5a479edc.chunk.js"
+  },
+  {
+    "revision": "d8fa00319cb55c6a4820",
+    "url": "/static/js/13.051fff2a.chunk.js"
+  },
+  {
+    "revision": "2ba1257b965dbc625cc9",
+    "url": "/static/js/14.41179ed5.chunk.js"
+  },
+  {
+    "revision": "0f44ab18e8c06b560ac4",
+    "url": "/static/js/15.afff5c2b.chunk.js"
+  },
+  {
+    "revision": "9234af0d9f185e862fb6",
+    "url": "/static/js/16.345058ed.chunk.js"
+  },
+  {
+    "revision": "46110121ae2e89a31e5f",
+    "url": "/static/js/17.17897b28.chunk.js"
+  },
+  {
+    "revision": "8bc9f39d9af11218ea74",
+    "url": "/static/js/18.4ccaf13d.chunk.js"
+  },
+  {
+    "revision": "f0f64cb3d2bd6e28fa3b",
+    "url": "/static/js/19.62367564.chunk.js"
+  },
+  {
+    "revision": "e95657347863792edd7e",
+    "url": "/static/js/2.accc1f29.chunk.js"
+  },
+  {
+    "revision": "8a4c74da13bd3378614e",
+    "url": "/static/js/20.3a3b73ea.chunk.js"
+  },
+  {
+    "revision": "80b4279d369dc0b03a53",
+    "url": "/static/js/3.c412a4ea.chunk.js"
+  },
+  {
+    "revision": "e7cad5e8c80a4a756b57f883a8b131b2",
+    "url": "/static/js/3.c412a4ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf72c6bcf5f5836aeec",
+    "url": "/static/js/4.26b406d0.chunk.js"
+  },
+  {
+    "revision": "4c011fe126b656013f3fc475e891f49c",
+    "url": "/static/js/4.26b406d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32c9de722041694f746a",
+    "url": "/static/js/7.ce81776c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/7.ce81776c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cda3370d927f9eae6eb5",
+    "url": "/static/js/8.2530a113.chunk.js"
+  },
+  {
+    "revision": "33c5093345150d4ad739",
+    "url": "/static/js/9.e0c335e8.chunk.js"
+  },
+  {
+    "revision": "a0395d657f6ae4e355bc",
+    "url": "/static/js/main.1731067e.chunk.js"
+  },
+  {
+    "revision": "a14255d10e286bf2dd95",
+    "url": "/static/js/runtime-main.9a340105.js"
+  },
+  {
+    "revision": "56bb51524222af2eb439344a048c9875",
+    "url": "/static/media/gas.56bb5152.jpg"
+  },
+  {
+    "revision": "8787c55d661e3751144ff38fa02cb00c",
+    "url": "/static/media/good-day.8787c55d.jpg"
+  },
+  {
+    "revision": "8233f5f99842a52da2f22310c8dbb9c2",
+    "url": "/static/media/good-night.8233f5f9.jpg"
+  },
+  {
+    "revision": "ab61dc3a12e51a9d405d9c8c2e2f958f",
+    "url": "/static/media/hazarduos.ab61dc3a.jpg"
+  },
+  {
+    "revision": "52c8dcb0b4d425cf1b3ea24b843a0a2b",
+    "url": "/static/media/hazy.52c8dcb0.png"
+  },
+  {
+    "revision": "52ccfa85df32f16a0abf7d69ffd8620e",
+    "url": "/static/media/heatmap_tmp.52ccfa85.png"
+  },
+  {
+    "revision": "9a9969a09a4e2c628175ed60c848c4c6",
+    "url": "/static/media/logo192.9a9969a0.png"
+  },
+  {
+    "revision": "ad9688c3d965be24f37ac0deb5ea51e3",
+    "url": "/static/media/logo192.ad9688c3.webp"
+  },
+  {
+    "revision": "198e98b7b75fa51fee622b12a3ed0219",
+    "url": "/static/media/me.198e98b7.jpg"
+  },
+  {
+    "revision": "393f2edda4d55648e927cf8b2925efad",
+    "url": "/static/media/me.393f2edd.webp"
+  },
+  {
+    "revision": "f0e85442e756ef47d6033135c7f5be36",
+    "url": "/static/media/moderate-day.f0e85442.jpg"
+  },
+  {
+    "revision": "fcfbd3dbb0a795ce01cb478d671623f7",
+    "url": "/static/media/moderate-night.fcfbd3db.jpg"
+  },
+  {
+    "revision": "d67ab07e604a4a18627db0eeaa1a5312",
+    "url": "/static/media/singapore-line.d67ab07e.png"
+  },
+  {
+    "revision": "0733819efc6a926de956ade8d2aca215",
+    "url": "/static/media/tropical.0733819e.webp"
+  },
+  {
+    "revision": "c6ef86659639046d57fdd360e507f453",
+    "url": "/static/media/tropical.c6ef8665.jpg"
+  },
+  {
+    "revision": "f7b35daee9112599c7a3df794f08e3d7",
+    "url": "/static/media/unhealthy-day.f7b35dae.jpg"
+  },
+  {
+    "revision": "04bc030af505601f064d77c542e3615d",
+    "url": "/static/media/unhealty-night.04bc030a.jpg"
+  },
+  {
+    "revision": "31ed53102dffcba3bc8f12f29766027f",
+    "url": "/static/media/very-unhealthy-day.31ed5310.jpg"
+  },
+  {
+    "revision": "e8d0ff4e29dd8d0df320730aa980bf0e",
+    "url": "/static/media/very-unhealthy-night.e8d0ff4e.jpg"
+  },
+  {
+    "revision": "142c15bdc4606c9c19be4bc1c1564f75",
+    "url": "/static/media/weather.142c15bd.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cc9f01ebc8802d23797b35380e89f9f4.js"
+  "/precache-manifest.3d9353e4f92477271fd74f685577f070.js"
 );
 
 self.addEventListener('message', (event) => {
